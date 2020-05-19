b1a6f64ae3ac7893770bd9ccc8f951deef832938
manicken <65423412+manicken@users.noreply.github.com>
2020-05-19 23:35:00 +0200

    Add files via upload

---
+++ UC_waveform.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace SequencerDemo
+{
+    partial class UC_waveform
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstBoxWaveformOscC = new System.Windows.Forms.ListBox();
+            this.lstBoxWaveformOscB = new System.Windows.Forms.ListBox();
+            this.lstBoxWaveformOscA = new System.Windows.Forms.ListBox();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.lblUidA = new System.Windows.Forms.Label();
+            this.lblUidC = new System.Windows.Forms.Label();
+            this.lblUidB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "OSC-C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "OSC-B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "OSC-A";
+            // 
+            // lstBoxWaveformOscC
+            // 
+            this.lstBoxWaveformOscC.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxWaveformOscC.FormattingEnabled = true;
+            this.lstBoxWaveformOscC.ItemHeight = 14;
+            this.lstBoxWaveformOscC.Items.AddRange(new object[] {
+            " 0 ",
+            " 1 ",
+            " 2 ",
+            " 3 ",
+            " 4 ",
+            " 5 ",
+            " 6 ",
+            " 7 ",
+            " 8 "});
+            this.lstBoxWaveformOscC.Location = new System.Drawing.Point(147, 36);
+            this.lstBoxWaveformOscC.Name = "lstBoxWaveformOscC";
+            this.lstBoxWaveformOscC.Size = new System.Drawing.Size(32, 130);
+            this.lstBoxWaveformOscC.TabIndex = 16;
+            // 
+            // lstBoxWaveformOscB
+            // 
+            this.lstBoxWaveformOscB.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxWaveformOscB.FormattingEnabled = true;
+            this.lstBoxWaveformOscB.ItemHeight = 14;
+            this.lstBoxWaveformOscB.Items.AddRange(new object[] {
+            " 0 ",
+            " 1 ",
+            " 2 ",
+            " 3 ",
+            " 4 ",
+            " 5 ",
+            " 6 ",
+            " 7 ",
+            " 8 "});
+            this.lstBoxWaveformOscB.Location = new System.Drawing.Point(109, 36);
+            this.lstBoxWaveformOscB.Name = "lstBoxWaveformOscB";
+            this.lstBoxWaveformOscB.Size = new System.Drawing.Size(32, 130);
+            this.lstBoxWaveformOscB.TabIndex = 17;
+            // 
+            // lstBoxWaveformOscA
+            // 
+            this.lstBoxWaveformOscA.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxWaveformOscA.FormattingEnabled = true;
+            this.lstBoxWaveformOscA.ItemHeight = 14;
+            this.lstBoxWaveformOscA.Items.AddRange(new object[] {
+            "SINE",
+            "SAWTOOTH",
+            "SQUARE",
+            "TRIANGLE",
+            "ARBITRARY",
+            "PULSE",
+            "SAW REVERSE",
+            "SAMPLE HOLD",
+            "VAR. TRIANGLE"});
+            this.lstBoxWaveformOscA.Location = new System.Drawing.Point(3, 36);
+            this.lstBoxWaveformOscA.Name = "lstBoxWaveformOscA";
+            this.lstBoxWaveformOscA.Size = new System.Drawing.Size(100, 130);
+            this.lstBoxWaveformOscA.TabIndex = 18;
+            // 
+            // lblMain
+            // 
+            this.lblMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMain.Location = new System.Drawing.Point(0, 0);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(187, 21);
+            this.lblMain.TabIndex = 22;
+            this.lblMain.Text = "Waveform";
+            this.lblMain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUidA
+            // 
+            this.lblUidA.AutoSize = true;
+            this.lblUidA.Location = new System.Drawing.Point(30, 102);
+            this.lblUidA.Name = "lblUidA";
+            this.lblUidA.Size = new System.Drawing.Size(35, 13);
+            this.lblUidA.TabIndex = 23;
+            this.lblUidA.Text = "label4";
+            // 
+            // lblUidC
+            // 
+            this.lblUidC.AutoSize = true;
+            this.lblUidC.Location = new System.Drawing.Point(144, 102);
+            this.lblUidC.Name = "lblUidC";
+            this.lblUidC.Size = new System.Drawing.Size(35, 13);
+            this.lblUidC.TabIndex = 23;
+            this.lblUidC.Text = "label4";
+            // 
+            // lblUidB
+            // 
+            this.lblUidB.AutoSize = true;
+            this.lblUidB.Location = new System.Drawing.Point(106, 102);
+            this.lblUidB.Name = "lblUidB";
+            this.lblUidB.Size = new System.Drawing.Size(35, 13);
+            this.lblUidB.TabIndex = 23;
+            this.lblUidB.Text = "label4";
+            // 
+            // UC_waveform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.lblUidB);
+            this.Controls.Add(this.lblUidC);
+            this.Controls.Add(this.lblUidA);
+            this.Controls.Add(this.lblMain);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxWaveformOscC);
+            this.Controls.Add(this.lstBoxWaveformOscB);
+            this.Controls.Add(this.lstBoxWaveformOscA);
+            this.Name = "UC_waveform";
+            this.Size = new System.Drawing.Size(187, 174);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstBoxWaveformOscC;
+        private System.Windows.Forms.ListBox lstBoxWaveformOscB;
+        private System.Windows.Forms.ListBox lstBoxWaveformOscA;
+        public System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.Label lblUidA;
+        private System.Windows.Forms.Label lblUidC;
+        private System.Windows.Forms.Label lblUidB;
+    }
+}
---
+++ UC_waveform.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SequencerDemo
+{
+    public partial class UC_waveform : UserControl
+    {
+        public delegate void SendHandler(int data1, int data2);
+        public event SendHandler SendData;
+
+        public void Send(int data1, int data2)
+        {
+            if (SendData == null)
+                return;
+            SendData(data1, data2);
+        }
+
+        public int uid_oscA = 0;
+        public int uid_oscB = 0;
+        public int uid_oscC = 0;
+
+        [Browsable(true)]
+        public int UID_A
+        {
+            get { return uid_oscA; }
+            set { uid_oscA = value; lblUidA.Text = "uid:" + value.ToString(); }
+        }
+        [Browsable(true)]
+        public int UID_B
+        {
+            get { return uid_oscB; }
+            set { uid_oscB = value; lblUidB.Text = "uid:" + value.ToString(); }
+        }
+        [Browsable(true)]
+        public int UID_C
+        {
+            get { return uid_oscC; }
+            set { uid_oscC = value; lblUidC.Text = "uid:" + value.ToString(); }
+        }
+
+        public void Hide_UidDesignerLbls()
+        {
+            lblUidA.Visible = false;
+            lblUidB.Visible = false;
+            lblUidC.Visible = false;
+        }
+
+        private bool valuesUpdating = false;
+
+        public UC_waveform()
+        {
+            InitializeComponent();
+
+            lstBoxWaveformOscA.MouseWheel += LstBoxWaveformSel_MouseWheel;
+            lstBoxWaveformOscB.MouseWheel += LstBoxWaveformSel_MouseWheel;
+            lstBoxWaveformOscC.MouseWheel += LstBoxWaveformSel_MouseWheel;
+            lstBoxWaveformOscA.SelectedIndex = 0;
+            lstBoxWaveformOscB.SelectedIndex = 0;
+            lstBoxWaveformOscC.SelectedIndex = 0;
+            lstBoxWaveformOscA.SelectedIndexChanged += lstBoxWaveformOscA_SelectedIndexChanged;
+            lstBoxWaveformOscB.SelectedIndexChanged += lstBoxWaveformOscB_SelectedIndexChanged;
+            lstBoxWaveformOscC.SelectedIndexChanged += lstBoxWaveformOscC_SelectedIndexChanged;
+        }
+
+        public bool SetValue(int uid, int value)
+        {
+            valuesUpdating = true;
+            if (uid == uid_oscA)
+            {
+                lstBoxWaveformOscA.SelectedIndex = value;
+                valuesUpdating = false;
+                return true;
+            }
+            else if (uid == uid_oscB)
+            {
+                lstBoxWaveformOscB.SelectedIndex = value;
+                valuesUpdating = false;
+                return true;
+            }
+            else if (uid == uid_oscC)
+            {
+                lstBoxWaveformOscC.SelectedIndex = value;
+                valuesUpdating = false;
+                return true;
+            }
+            valuesUpdating = false;
+            return false;
+        }
+
+        private void lstBoxWaveformOscA_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (valuesUpdating) return;
+            SendData(uid_oscA, lstBoxWaveformOscA.SelectedIndex);
+        }
+
+        private void lstBoxWaveformOscB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (valuesUpdating) return;
+            SendData(uid_oscB, lstBoxWaveformOscB.SelectedIndex);
+        }
+
+        private void lstBoxWaveformOscC_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (valuesUpdating) return;
+            SendData(uid_oscC, lstBoxWaveformOscC.SelectedIndex);
+        }
+
+        private void LstBoxWaveformSel_MouseWheel(object sender, MouseEventArgs e)
+        {
+            ((HandledMouseEventArgs)e).Handled = true;
+            ListBox lb = (ListBox)sender;
+            if (e.Delta > 0)
+            {
+                if (lb.SelectedIndex != 0)
+                    lb.SelectedIndex--;
+            }
+            else if (e.Delta < 0)
+            {
+                if (lb.SelectedIndex != (lb.Items.Count - 1))
+                    lb.SelectedIndex++;
+            }
+        }
+    }
+}
